0700cd1ed07254d71836aa3909dab304dcf70ae1
Nopearino <19935@stu.ipbeja.pt>
2024-04-04 16:29:17 +0000

    img

--- trabalho/mand.c
+++ trabalho/mand.c
@@ -15,25 +15,21 @@ typedef struct {
 } Color;
 
 Color getColor(int iter){
-    Color colors[] = {
-        {66, 30, 15}, {25, 7, 26}, {9, 1, 47}, {4, 4, 73},
-        {0, 7, 100}, {12, 44, 138}, {24, 82, 177}, {57, 125, 209},
-        {134, 181, 229}, {211, 236, 248}, {241, 233, 191}, {248, 201, 95},
-        {255, 170, 0}, {204, 128, 0}, {153, 87, 0}, {106, 52, 3}
-    };
-    int numColors = sizeof(colors)/sizeof(colors[0]);
     Color black;
     black.r = 0;
     black.g = 0;
     black.b = 0;
-    //if(iter > 150){
-        if(iter >= MAX_ITER)
-            return colors[numColors - 1];
-        else 
-            return colors[iter % numColors];
-    //}else{
-    //    return black;
-    //}
+    
+    Color colors[] = {
+        black, black, black, black,
+        black, black, black, black,
+        {134, 181, 229}, {211, 236, 248}, {241, 233, 191}, {248, 201, 95},
+        {255, 170, 0}, {204, 128, 0}, {153, 87, 0}, black}
+    };
+    int numColors = sizeof(colors)/sizeof(colors[0]);
+   
+    
+    return colors[iter % numColors];
 }
 
 void mand(unsigned char *output){
